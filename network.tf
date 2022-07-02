5fd19f6b0de311b9740bab3fc2fa369c9b776088
KhanhLinh <linhndk94@gmail.com>
2022-07-03 06:13:58 +0700

    added a bunch of network configurations, only security list left

--- network.tf
+++ network.tf
@@ -3,3 +3,42 @@ resource "oci_core_vcn" "po_vcn" {
   cidr_blocks    = var.vcn_cidr_blocks
   display_name   = "po-vcn"
 }
+
+resource "oci_core_dhcp_options" "po_dhcp" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.po_vcn.id
+  options {
+    type        = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
+  }
+  display_name   = "po_dhcp"
+}
+
+resource "oci_core_internet_gateway" "po_internet_gateway" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.po_vcn.id
+  display_name   = "po-ig"
+}
+
+resource "oci_core_route_table" "po_route_table" {
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.po_vcn.id
+  display_name   = "po-rt"
+
+  route_rules {
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.po_internet_gateway.id
+  }
+}
+
+#todo setup security list
+
+resource "oci_core_subnet" "po_subnet" {
+  compartment_id  = var.tenancy_ocid
+  cidr_block      = var.subnet_cidr_blocks
+  vcn_id          = oci_core_vcn.po_vcn.id
+  display_name    = "po-subnet"
+  dhcp_options_id = oci_core_dhcp_options.po_dhcp.id
+  route_table_id  = oci_core_route_table.po_route_table.id
+}
--- vars.tf
+++ vars.tf
@@ -14,6 +14,9 @@ variable "oci_ubuntu_amd64_image" {
   default = "ocid1.image.oc1.ap-singapore-1.aaaaaaaa5fiuyidchi6v3qmdlmu7qmbgug6d22gvlzmroic3s3d5dqxr76ea"
 }
 variable "vcn_cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_blocks" {
+  default = "10.0.1.0/24"
 }
